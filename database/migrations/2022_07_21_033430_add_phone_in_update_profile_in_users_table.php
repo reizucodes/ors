ecf4acd917156398284e3758fea13c8f10b313dd
reizucodes <blaisebueno@gmail.com>
2022-07-25 12:55:44 +0800

    new repo

---
+++ database/migrations/2022_07_21_033430_add_phone_in_update_profile_in_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //Add phone column to store phone in update profile 
+            $table->bigInteger('phone')->nullable()->after('last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //Drop column
+            $table->dropColumn('phone');
+        });
+    }
+};
---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+import '../../node_modules/flowbite/src/flowbite.js';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
